40be608c12e12cdfa35c61cccde2980e31f8ccb8
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-02-05 17:37:41 +0530

    phone done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("test");
+// console.log("test");
+document.getElementById("order1").onclick = function() {order(1)};
+document.getElementById("order2").onclick = function() {order(2)};
+document.getElementById("order3").onclick = function() {order(3)};
+let orderlist=[];
+function order(item_num){
+    orderlist.push(item_num);
+    console.log("ordered "+item_num);
+    return 0;
+}

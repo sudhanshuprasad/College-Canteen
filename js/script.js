fd5fc74e89bd5e46e04a9d0dcacbf6b5d8f36a80
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-02-10 23:35:12 +0530

    bug fix

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ function order(item_num){
     console.log("ordered "+item_num);
     return 0;
 }
+// document.getElementById("more").onclick=function(){
+//     document.getElementById("more").innerHTML=`<li><a href="#5">Shop 5</a></li>
+//     <li><a href="#6">Shop 6</a></li>
+//     <li><a href="#7">Shop 7</a></li>
+//     <li><a href="#8">Shop 8</a></li>
+//     <li><a href="#9">Shop 9</a></li>`;
+// }

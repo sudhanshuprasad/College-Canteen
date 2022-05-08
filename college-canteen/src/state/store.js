583be63919b511a8635823be87bb77f0bcebd683
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-05-09 03:14:49 +0530

    redux store should work

--- college-canteen/src/state/store.js
+++ college-canteen/src/state/store.js
@@ -1,5 +1,5 @@
-import { applyMiddlewere, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk'
 import reducers from "./reducers";
 
-export const store=createStore(reducers, {}, applyMiddlewere(thunk));
+export const store=createStore(reducers, {}, applyMiddleware(thunk));
